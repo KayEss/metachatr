7001d73d89cccbb80e224cd6179857f3e732732c
Kirit Sælensminde <k@kirit.com>
2011-10-22 21:20:54 +0700

    Added a new empty library for the actual runtime.

---
+++ Cpp/metachatr-runtime/filehandler.cpp
@@ -0,0 +1,8 @@
+/*
+    Copyright 2011, Kirit Saelensminde. http://www.kirit.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
